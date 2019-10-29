3b2f40257c13b5d0584c1c6110f1d741ca3d4f96
ERU <enteocode@gmail.com>
2019-10-29 18:20:52 +0100

    Updated dependencies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import AppContainer from 'component/AppContainer';
 import { Provider } from 'react-redux';
-import { hot } from 'react-hot-loader';
+import { hot } from 'react-hot-loader/root';
 import './index.scss';
 
 import type { Component } from 'react';
@@ -27,4 +27,4 @@ const App = ({ store }: Props): Component => (
     </Provider>
 );
 
-export default hot(module)(App);
+export default hot(App);
--- webpack.config.js
+++ webpack.config.js
@@ -12,14 +12,15 @@
  * @private
  */
 
-const webpack = require('webpack');
-const fs = require('fs');
 const path = require('path');
-const autoprefixer = require('autoprefixer');
 
-// Constants
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
-const MODE_PRODUCTION  = 'production';
+// Definitions
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
 const MODE_DEVELOPMENT = 'development';
 const HOST = 'localhost';
 const PORT = 3000;
@@ -37,168 +38,171 @@ const resolve = (... localPath) => path.resolve(__dirname, ... localPath);
  * Returns the proper hashing format of CSS Module classes
  *
  * @private
- * @param {string} mode
+ * @param {string} isDevelopment
  * @return {string}
  */
-const getLocalClassName = (mode) => {
-    return mode === MODE_DEVELOPMENT ? '[local]__[md5:hash:base62:4]' : '[sha256:hash:base62:6]';
+const getLocalIdentName = (isDevelopment) => {
+    return isDevelopment ? '[local]__[md5:hash:4]' : '[md5:hash:6]';
 };
 
 /**
- * Returns with the mode-enhanced list of Babel plugins
- *
- * @private
- * @param {boolean} isDevelopment
- * @return {string[]}
+ * @type {Object}
  */
-const getBabelPlugins = (isDevelopment) => {
-    const file = fs.readFileSync('./.babelrc', 'utf-8');
-    const json = JSON.parse(file);
-    const data = json.plugins || [];
-
-    if (isDevelopment) {
-        return ['react-hot-loader/babel'].concat(data);
-    }
-    return data;
-};
-
-/**
- * Configurator
- *
- * We're returning with a function instead of a plain Object
- * to get the mode for actual runtime.
- *
- * @param {string} env
- * @param {string} mode
- * @return {Object}
- */
-module.exports = (env, { mode = MODE_DEVELOPMENT }) => {
-    const isDevelopment = mode === MODE_DEVELOPMENT;
-
-    const config = {
-        devtool : isDevelopment ? 'inline-source-map' : void 0,
-
-        mode,
-
-        entry : {
-            app : [ 'whatwg-fetch', './src/bootstrap.js' ]
-        },
-        output : {
-            filename : '[name].js',
-            chunkFilename : '[name].js?v=[chunkhash]',
-            path : resolve('docs/static'),
-            publicPath : '/static/'
-        },
-        optimization : {
-            splitChunks : {
-                cacheGroups : {
-                    vendor : {
-                        test : /node_modules/,
-                        name : 'vendor',
-                        chunks : 'all'
+module.exports = {
+    devtool : isDevelopment ? 'inline-source-map' : void 0,
+    stats : {
+        children : false
+    },
+    entry : {
+        app : [ 'react-hot-loader/patch', 'whatwg-fetch', './src/bootstrap.js' ]
+    },
+    output : {
+        filename : '[name].js',
+        path : resolve('docs/static'),
+        publicPath : '/static/'
+    },
+    optimization : {
+        minimizer : [
+            new TerserPlugin({
+                parallel : true,
+                cache : true,
+                terserOptions : {
+                    parse : {
+                        ecma : 8
+                    },
+                    compress : {
+                        drop_console : true,
+                        ecma : 5,
+                        warnings : false,
+                        passes : 3
+                    },
+                    mangle : {
+                        safari10 : true
+                    },
+                    output : {
+                        ascii_only : false,
+                        ecma : 5,
+                        comments : false
                     }
-                }
-            }
-        },
-        devServer : {
-            contentBase : resolve('docs'),
-            clientLogLevel : 'warning',
-            host : HOST,
-            port : PORT,
-            historyApiFallback : true,
-            hot : true,
-            stats : {
-                chunks : false
-            }
-        },
-        externals : {
-            'mapbox-gl' : 'mapboxgl'
-        },
-        node : {
-            fs : 'empty'
-        },
-        resolve : {
-            // To make the traversing easier, we creating aliases for all of the logical groups
-
-            alias : {
-                action    : resolve('src/action'),
-                component : resolve('src/component'),
-                lib       : resolve('src/lib'),
-                reducer   : resolve('src/reducer')
-            }
-        },
-        plugins : [
-            // We want to force compilation targets by the mode given instead of the environmental variables,
-            // so we can't use EnvironmentPlugin
-
-            new webpack.DefinePlugin({
-                'process.env.NODE_ENV' : JSON.stringify(isDevelopment ? MODE_DEVELOPMENT : MODE_PRODUCTION),
-                'DEBUG' : JSON.stringify(isDevelopment)
+                },
             })
         ],
-        module : {
-            rules : [
-                {
-                    test : /\.jsx?$/,
-                    exclude : /node_modules/,
-                    use : [
-                        {
-                            loader : 'babel-loader',
-                            options : {
-                                cacheDirectory : true,
-                                plugins : getBabelPlugins(isDevelopment)
-                            }
-                        },
-                        {
-                            loader : 'eslint-loader'
-                        }
-                    ]
-                },
-                {
-                    test : /\.s?css$/,
-                    use : [
-                        {
-                            loader : 'style-loader',
-                            options : {
-                                hmr : isDevelopment,
-                                sourceMap : isDevelopment
-                            }
-                        },
-                        {
-                            loader : 'css-loader',
-                            options : {
-                                localIdentName : getLocalClassName(mode),
-                                importLoaders : 2,
-                                modules : true,
-                                minimize : true
-                            }
-                        },
-                        {
-                            loader : 'sass-loader',
-                            options : {
-                                sourceMap : true
-                            }
-                        },
-                        {
-                            loader : 'postcss-loader',
-                            options : {
-                                sourceMap : true,
-                                plugins : [
-                                    autoprefixer({ browsers : [ '> 5%', 'Firefox ESR' ] })
-                                ]
-                            }
-                        }
-                    ]
+        splitChunks : {
+            cacheGroups : {
+                name : false,
+                vendor : {
+                    test : /node_modules/,
+                    name : 'vendor',
+                    chunks : 'all'
                 }
-            ]
+            }
         }
-    };
+    },
+    devServer : {
+        disableHostCheck : true,
+        inline : true,
+        contentBase : resolve('docs'),
+        clientLogLevel : 'warning',
+        host : HOST,
+        port : PORT,
+        historyApiFallback : true,
+        https : true,
+        hot : true,
+        headers : {
+            'P3P' : 'CP="ALL IND DSP COR ADM CONo CUR CUSo IVAo IVDo PSA PSD TAI TELo OUR SAMo CNT COM INT NAV ONL PHY PRE PUR UNI"',
+            'Access-Control-Allow-Origin' : '*',
+            'Access-Control-Allow-Credentials' : 'true'
+        },
+        stats : 'errors-only'
+    },
+    externals : {
+        'mapbox-gl' : 'mapboxgl'
+    },
+    node : {
+        fs : 'empty'
+    },
+    resolve : {
+        // To make the traversing easier, we creating aliases for all of the logical groups
 
-    // Hot Module Replacement
-
-    if (isDevelopment) {
-        config.plugins.unshift(new webpack.HotModuleReplacementPlugin());
+        alias : {
+            action    : resolve('src/action'),
+            component : resolve('src/component'),
+            lib       : resolve('src/lib'),
+            reducer   : resolve('src/reducer')
+        }
+    },
+    plugins : [
+        new MiniCssExtractPlugin({ filename : '[name].css' })
+    ],
+    module : {
+        strictExportPresence : true,
+        rules : [
+            {
+                test : /\.js$/,
+                exclude : /node_modules/,
+                use : [
+                    {
+                        loader : 'babel-loader',
+                        options : {
+                            cacheDirectory : true
+                        }
+                    },
+                    {
+                        loader : 'eslint-loader',
+                        options : {
+                            failOnWarning : isDevelopment,
+                            cache : false
+                        }
+                    }
+                ]
+            },
+            {
+                test : /\.s?css$/,
+                use : [
+                    ! isDevelopment ? MiniCssExtractPlugin.loader : {
+                        loader : 'style-loader'
+                    },
+                    {
+                        loader : 'css-loader',
+                        options : {
+                            importLoaders : 3,
+                            modules : {
+                                localIdentName : getLocalIdentName(isDevelopment)
+                            }
+                        }
+                    },
+                    {
+                        loader : 'resolve-url-loader'
+                    },
+                    {
+                        loader : 'postcss-loader',
+                        options : {
+                            sourceMap : true,
+                            plugins : [
+                                require('autoprefixer'),
+                                require('cssnano')({ preset : 'default' })
+                            ]
+                        }
+                    },
+                    {
+                        loader : 'sass-loader',
+                        options : {
+                            sourceMap : isDevelopment
+                        }
+                    }
+                ]
+            },
+            {
+                test : /\.(jpe?g|png|gif|svg|ttf|eot|otf|woff2?)$/,
+                use : [
+                    {
+                        loader : 'file-loader',
+                        options : {
+                            name : '[name].[ext]?v=[hash:8]'
+                        }
+                    }
+                ]
+            }
+        ]
     }
-
-    return config;
 };
